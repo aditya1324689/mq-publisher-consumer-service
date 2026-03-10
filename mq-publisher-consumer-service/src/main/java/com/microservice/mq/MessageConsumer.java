24daceafaedda5c7fb298e2c246587a0cc2ee2c4
aditya1324689 <adityadesh2495@gmail.com>
2026-03-10 18:00:36 +0530

    updated comments

--- mq-publisher-consumer-service/src/main/java/com/microservice/mq/MessageConsumer.java
+++ mq-publisher-consumer-service/src/main/java/com/microservice/mq/MessageConsumer.java
@@ -4,15 +4,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * MessageConsumer is component that retrieves (consumes) messages from the in-memory queue
+ * managed by the MessagePublisher.
+ *
+ * Responsibilities:
+ * - Poll messages from the queue
+ * - Log consumed messages for traceability
+ * - Return the consumed message to the caller
+ */
 @Component
 public class MessageConsumer {
+	// tracking consumption operations---logger
 	private static final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
+	// adding dependency of publisher service that manages the queue.
 	private final MessagePublisher publisher;
 
 	public MessageConsumer(MessagePublisher publisher) {
 		this.publisher = publisher;
 	}
 
+	/**
+     * consuming a message from the queue.
+     * - Retrieves the next message using publisher.pollMessage()
+     * - Logs the consumed message if available
+     * - Returns consumed message, or null if queue is empty
+     */
 	public String consume() {
 		String message = publisher.pollMessage();
 		if (message != null) {
