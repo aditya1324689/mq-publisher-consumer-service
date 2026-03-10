ae38cf1a7b604c6ac60666b5ba911ede598113cd
aditya1324689 <adityadesh2495@gmail.com>
2026-03-10 17:53:33 +0530

    updated comments

--- mq-publisher-consumer-service/src/main/java/com/microservice/mq/PublisherController.java
+++ mq-publisher-consumer-service/src/main/java/com/microservice/mq/PublisherController.java
@@ -4,15 +4,31 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * PublisherController is a REST controller that exposes
+ * an endpoint for publishing messages to the in-memory queue.
+ *
+ * Responsibilities:
+ * - Accept JSON payloads via POST requests
+ * - Extract message content from the payload
+ * - Delegate publishing to the MessagePublisher service
+ * - Return appropriate HTTP responses (success or error)
+ */
 @RestController
 @RequestMapping("/publish")
 public class PublisherController {
+	// service which is handling queue operations
 	private final MessagePublisher publisher;
 
+	// ensuring the publisher service is available using constructor
 	public PublisherController(MessagePublisher publisher) {
 		this.publisher = publisher;
 	}
 
+	/**
+     * REST endpoint to publish a message, accepts a JSON payload, converts it to a string,
+     * and sends it to the MessagePublisher also handled unexpected errors
+     */
 	@PostMapping
 	public ResponseEntity<String> publishMessage(@RequestBody Map<String, Object> payload) {
 		try {
